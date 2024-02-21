06f48f8fd8c63e15ec79260aaeb9d95d433279de
Pablo Valdes <pablovaldes0925@gmail.com>
2024-02-20 22:50:06 -0500

    spaghetti

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className='flex flex-col justify-center items-center mb-20 w-auto'>
         <h1 className="font-bold text-center text-5xl">Upload and share your images</h1>
         <h2 className='text-3xl text-center mt-3 text-gray-600'>Upload images, get unique urls, and share anywhere online for free.</h2>
-          <Link href='/sign-up'>
+          <Link href='/sign-in'>
             <button className='px-9 py-4 mt-5 bg-purple-900 rounded-md text-white tracking-wider'>
               START UPLOADING
             </button>
